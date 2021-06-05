c30d4238f8409567fd37be00a8d5f97b8de91b36
CristianAlfaro <00167917@uca.edu.sv>
2021-06-04 22:34:23 -0600

    REQUEST AND RESPONSE

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-  res.send(
+  res.render('index', { title: 'Tempest' });
+ /* res.send(
     {
       data: {
         user: 'my_user',
@@ -14,6 +14,22 @@ router.get('/', function(req, res, next) {
       meta: {
         size: 23,
         page: 32,
+      },
+    }
+  );*/
+});
+
+router.get('/user', function(req, res, next) {
+  res.send(
+    {
+      data: {
+        user: 'my_user',
+        title: 'main page',
+        size: 99,
+      },
+      meta: {
+        size: 99,
+        page: 99,
       },
     }
   );
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/user', function(req, res, next) {
+  res.send(
+    {
+      data: {
+        user: 'my_user',
+        title: 'main page',
+        size: 99,
+      },
+      meta: {
+        size: 99,
+        page: 99,
+      },
+    }
+  );
 });
 
 module.exports = router;
